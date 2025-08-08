635ec425de31b3c79cc9fdca38133d934d5856a1
Sjoerd Zaalberg van Zelst <sjoerd@zzid.nl>
2025-08-08 14:53:04 +0100

    Enemy fire implementeren

--- main.ts
+++ main.ts
@@ -1,30 +1,4 @@
-function handleNebula() {
-    // Shift the brightness values down by one row
-    // Store the brightness of the last row
-    lastBrightness = rowBrightness[rowBrightness.length - 1]
-    for (let i = rowBrightness.length - 1; i > 0; i--) {
-        rowBrightness[i] = rowBrightness[i - 1]
-    }
-    // Wrap the last row's brightness to the first row
-    // Wrap the last row's brightness to the first row
-    rowBrightness[0] = lastBrightness
-    // Apply the brightness to each row in the nebula
-    // for (let y2 = 0; y2 <= background_nebula.length - 1; y2++) {
-    //     bright = rowBrightness[y2]
-    //     for (let sprite of background_nebula[y2]) {
-    //         sprite.set(LedSpriteProperty.Brightness, bright)
-    //     }
-    // }
-}
-
-loops.everyInterval(200, function () {
-    for (let index = 0; index <= 4; index++) {
-        // handleNebula()
-        // handleStar(index)
-    }
-})
-
-function UpdateSpace() {
+function UpdateSpace () {
     for (let bullet of ActiveBullets) {
         if (bullet.get(LedSpriteProperty.Y) > 0) {
             bullet.change(LedSpriteProperty.Y, -1)
@@ -34,6 +8,21 @@ function UpdateSpace() {
         }
         if (bullet.isTouching(SpaceDestroyer)) {
             EnemyExplode()
+        }
+    }
+    for (let enemyBullet of ActiveEnemyFire) {
+        if (enemyBullet.get(LedSpriteProperty.Y) < 5) {
+            enemyBullet.change(LedSpriteProperty.Y, 1)
+            enemyBullet.change(LedSpriteProperty.Brightness, -30)
+        } else {
+            enemyBullet.change(LedSpriteProperty.Brightness, -60)
+        }
+        if (enemyBullet.get(LedSpriteProperty.Y) == 5) {
+            enemyBullet.delete()
+        }
+        if (enemyBullet.isTouching(Spaceship)) {
+            music.play(music.builtinPlayableSoundEffect(soundExpression.sad), music.PlaybackMode.InBackground)
+            enemyBullet.delete()
         }
     }
     for (let particle of ExplosionParticles) {
@@ -46,7 +35,7 @@ function UpdateSpace() {
         }
     }
 }
-function showMenuItem(item: string) {
+function showMenuItem (item: string) {
     if (item == "START") {
         basic.showLeds(`
             . . . . .
@@ -85,10 +74,10 @@ function showMenuItem(item: string) {
             `)
     }
 }
-function SpaceShipFire() {
+function SpaceShipFire () {
     ActiveBullets.push(game.createSprite(Spaceship.get(LedSpriteProperty.X), Spaceship.get(LedSpriteProperty.Y) - 1))
 }
-function EnemyExplode() {
+function EnemyExplode () {
     for (let direction of ExplosionParticleDirections) {
         temp_particle = game.createSprite(SpaceDestroyer.get(LedSpriteProperty.X), 0)
         temp_particle.set(LedSpriteProperty.Direction, direction)
@@ -97,12 +86,12 @@ function EnemyExplode() {
     SpaceDestroyer.delete()
     spawnSpaceDestroyer()
 }
-function PlaySound(sound: string) {
+function PlaySound (sound: string) {
     if (soundOn) {
-
+    	
     }
 }
-function DeleteGarbage() {
+function DeleteGarbage () {
     for (let finished_bullet of ActiveBullets) {
         if (finished_bullet.get(LedSpriteProperty.Y) == 0 && finished_bullet.get(LedSpriteProperty.Brightness) == 0) {
             finished_bullet.delete()
@@ -120,25 +109,12 @@ function DeleteGarbage() {
         }
     }
 }
-function startGame() {
+function startGame () {
     menuActive = false
     game.setLife(5)
     Spaceship = game.createSprite(2, 4)
     spawnSpaceDestroyer()
-}
-function initNebula() {
-    let nebula: game.LedSprite[][] = []
-    for (let y = 0; y <= 4; y++) {
-        let row: game.LedSprite[] = []
-        for (let x = 0; x <= 4; x++) {
-            sprite = game.createSprite(x, y)
-            sprite.set(LedSpriteProperty.Brightness, 0)
-            sprite.set(LedSpriteProperty.Direction, 180)
-            row.push(sprite)
-        }
-        nebula.push(row)
-    }
-    return nebula
+    gameStarted = true
 }
 input.onButtonPressed(Button.A, function () {
     if (menuActive) {
@@ -166,7 +142,7 @@ input.onButtonPressed(Button.A, function () {
 // nebulaRow = 0
 // }
 // }
-function confirmMenu() {
+function confirmMenu () {
     if (MenuItems[menuIndex] == "SOUND") {
         if (soundOn) {
             soundOn = false
@@ -212,17 +188,17 @@ input.onButtonPressed(Button.B, function () {
         Spaceship.change(LedSpriteProperty.X, 1)
     }
 })
-function initMenu() {
+function initMenu () {
     menuActive = true
     MenuItems = ["START", "SOUND", "SPEED"]
     menuIndex = 0
     menuActive = true
     showMenuItem("START")
 }
-function spawnSpaceDestroyer() {
+function spawnSpaceDestroyer () {
     SpaceDestroyer = game.createSprite(randint(0, 4), 0)
 }
-function navigateMenu(direction: number) {
+function navigateMenu (direction: number) {
     menuIndex += direction
     if (menuIndex < 0) {
         menuIndex = MenuItems.length - 1
@@ -232,29 +208,12 @@ function navigateMenu(direction: number) {
     }
     showMenuItem(MenuItems[menuIndex])
 }
-function initStars() {
-    let StarSprites: game.LedSprite[] = []
-    starPosY = [
-        4,
-        8,
-        16,
-        12,
-        0
-    ]
-    for (let x2 = 0; x2 <= 4; x2++) {
-        StarSprites.push(game.createSprite(x2, starPosY[x2]))
-    }
-    for (let p of StarSprites) {
-        p.set(LedSpriteProperty.Direction, 180)
-    }
+function EnemyFire () {
+    ActiveEnemyFire.push(game.createSprite(SpaceDestroyer.get(LedSpriteProperty.X), SpaceDestroyer.get(LedSpriteProperty.Y) + 1))
 }
-function EnemyFire() {
-
-}
-let starPosY: number[] = []
 let menuIndex = 0
 let MenuItems: string[] = []
-let sprite: game.LedSprite = null
+let gameStarted = false
 let menuActive = false
 let temp_particle: game.LedSprite = null
 let Spaceship: game.LedSprite = null
@@ -263,40 +222,36 @@ let ActiveBullets: game.LedSprite[] = []
 let ExplosionParticleDirections: number[] = []
 let soundOn = false
 let ExplosionParticles: game.LedSprite[] = []
+let ActiveEnemyFire: game.LedSprite[] = []
 let temp_pos = 0
-let lastBrightness = 0
-let bright = 0
-let temp_star = null
-let nebulaRow = 0
-// Array to hold brightness values for each row
-let rowBrightness = [
-    10,
-    50,
-    20,
-    60,
-    20
-]
+ActiveEnemyFire = []
 ExplosionParticles = []
 let menuSpeed = 100
 let gameSpeed = 100
 let enemySpeed = 200
+let enemyFireSpeed = 3500
 let garbageCollectorSpeed = 3000
 // let background_nebula = initNebula()
 soundOn = true
 ExplosionParticleDirections = [
-    -135,
-    135,
-    45,
-    -45
+-135,
+135,
+45,
+-45
 ]
 initMenu()
 loops.everyInterval(garbageCollectorSpeed, function () {
     DeleteGarbage()
 })
+loops.everyInterval(enemyFireSpeed, function () {
+    if (gameStarted) {
+        EnemyFire()
+    }
+})
 // loops.everyInterval(menuSpeed, function () {
 // })
 loops.everyInterval(gameSpeed, function () {
-    if (!(menuActive)) {
+    if (gameStarted) {
         UpdateSpace()
     }
 })
