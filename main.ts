57e73da937aafcb9e1df3bf3cade9c9abae91282
Sjoerd Zaalberg van Zelst <sjoerd@zzid.nl>
2025-08-12 16:09:25 +0100

    Remove stale code blocks

--- main.ts
+++ main.ts
@@ -486,7 +486,6 @@ let ExplosionParticleDirections: number[] = []
 let ExplosionParticles: game.LedSprite[] = []
 let introActive = false
 let soundOn = false
-let Enemy_Better = 0
 let temp_pos = 0
 let ActiveEnemyFire : game.LedSprite[] = []
 music.setBuiltInSpeakerEnabled(true)
@@ -509,18 +508,8 @@ introTrailer()
 loops.everyInterval(garbageCollectorSpeed, function () {
     DeleteGarbage()
 })
-loops.everyInterval(1000, function () {
-    if (Enemy_Better == 2) {
-        EnemyFire()
-    }
-})
 loops.everyInterval(enemyFireSpeed, function () {
     if (gameStarted) {
-        EnemyFire()
-    }
-})
-loops.everyInterval(2000, function () {
-    if (Enemy_Better == 1) {
         EnemyFire()
     }
 })
@@ -530,9 +519,6 @@ loops.everyInterval(gameSpeed, function () {
     }
 })
 basic.forever(function () {
-    if (score == 10) {
-        Enemy_Better += 1
-    }
     if (game.isGameOver()) {
         basic.showNumber(score)
         control.reset()
